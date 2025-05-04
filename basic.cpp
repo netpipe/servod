348b94404e743a67a8d690c6b5e8593011d5a38d
Clay Shippy <teslaenergy@gmail.com>
2025-05-03 22:58:18 -0600

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -15,6 +15,7 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sqlite3.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 8192
@@ -39,6 +40,34 @@ std::string get_mime_type(const std::string& path) {
     if (path.ends_with(".gif")) return "image/gif";
     if (path.ends_with(".php")) return "text/html";
     return "text/plain";
+}
+
+void handle_sql_query(SSL* ssl, const std::string& query) {
+    sqlite3* db;
+    sqlite3_stmt* stmt;
+
+    if (sqlite3_open("webdata.db", &db) != SQLITE_OK) {
+        send_response(ssl, "500 Internal Server Error", "text/plain", "Database error");
+        return;
+    }
+
+    std::ostringstream result;
+    if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+        int cols = sqlite3_column_count(stmt);
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            for (int i = 0; i < cols; ++i) {
+                result << sqlite3_column_name(stmt, i) << "="
+                       << (const char*)sqlite3_column_text(stmt, i) << "; ";
+            }
+            result << "\\n";
+        }
+    } else {
+        result << "SQL error: " << sqlite3_errmsg(db);
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    send_response(ssl, "200 OK", "text/plain", result.str());
 }
 
 void handle_php_cgi(int client_fd, const std::string& path, const std::string& query_string) {
@@ -96,6 +125,12 @@ void handle_client(int client_fd) {
         query_string = url.substr(qs_pos + 1);
         url = url.substr(0, qs_pos);
     }
+    
+if (url.starts_with("/query") && method == "GET") {
+    std::string sql = query_string.substr(query_string.find("sql=") + 4);
+    sql = url_decode(sql);  // You'll need a helper to decode %20 etc.
+    handle_sql_query(ssl, sql);
+}
 
     std::string filepath = WEBROOT + url;
     if (filepath.back() == '/') filepath += "index.html";
